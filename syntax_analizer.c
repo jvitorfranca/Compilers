7a3a8b0cbc1abba035f47b3db6ae73c421bf9baa
jvitorfranca <vitoorbr@gmail.com>
2018-10-10 15:50:11 -0300

    adding syntax analizer

--- syntax_analizer.c
+++ syntax_analizer.c
@@ -5,52 +5,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct _tree_{
-    void* key;
-    struct _tree_ *left;
-    struct _tree_ *right;
-}Tree;
-
 char token;
-
-Tree* createNode(void* token){
-
-    Tree *node;
-
-    node = (Tree*)malloc(sizeof(Tree));
-
-    if (node != NULL){
-
-        node->left = NULL;
-        node->right = NULL;
-        node->key = token;
-
-        return node;
-
-    }
-
-    return NULL;
-}
-
-void left(Tree *node, void* temp){
-    
-    Tree* newnode = createNode(temp);
-
-    if (newnode != NULL){
-        node->left = newnode;
-    }
-
-}
-
-void right(Tree *node, void* temp){
-
-    Tree* newnode = createNode(temp);
-    
-    if (newnode != NULL){
-        node->right = newnode;
-    }
-    
-}
 
 void error(){
     fprintf(stderr, "Error\n");
@@ -111,52 +66,6 @@ int exp(void){
     return temp;
 }
 
-/*
-Tree* expTree(void){
-    
-    Tree *temp, *novatemp;
-
-    temp = createNode((void*)term());
-
-    while (token == '+' || token == '-'){
-        switch (token){
-            case '+':
-                match('+');
-                novatemp = createNode((void*)token);
-                left(novatemp, (void*)temp);
-                right(novatemp, (void*)term());
-                temp = novatemp;
-                break;
-            case '-':
-                match('-');
-                novatemp = createNode((void*)token);
-                left(novatemp, (void*)temp);
-                right(novatemp, (void*)term());
-                temp = novatemp;
-                break;
-        }
-    }
-    return temp;
-}
-*/
-
-Tree* expTree(void){
-
-    Tree* temp, *novatemp;
-
-    temp = createNode((void*)term());
-
-    while (token == '+' || token == '-'){
-        novatemp = createNode((void*)token);
-        match(token);
-        left(novatemp, (void*)temp->key);
-        right(novatemp, (void*)term());
-        temp = novatemp;
-    }
-
-    return temp;
-}
-
 int factor(void){
     int temp;
 
@@ -175,37 +84,17 @@ int factor(void){
     return temp;
 }
 
-void preOrder(Tree *node){
-    
-    if (node != NULL){
-        if (node->key == (void*)'+' || node->key == (void*)'-'){
-            printf("%c\n", (char*)node->key);
-        } else {
-            printf("%d\n", (int)node->key);
-        }
-        preOrder(node->left);
-        preOrder(node->right);
-    }
-}
-
 int main(int argc, char const *argv[])
 {
     int result;
     token = getchar();
 
-    Tree* result1;
-    result1 = expTree();
-
-    preOrder(result1);
-
-    /*
     result = exp();
     if (token == '\n'){
         printf("Result = %d\n", result);
     } else {
         error();
     }
-    */
 
     return 0;
 }
